b3fb8634c9cb2a40335a3877db008be045733047
Randy Topliffe <randytopliffe@gmail.com>
2018-05-09 16:05:02 -0400

    Symbol Code Examples
    - Add symbol code examples
    - Add html file to run the examples in the browser

---
+++ src/run_button.js
@@ -0,0 +1,40 @@
+const EventHandler = {
+  handleEvent(e) {
+    this[`on${e.type}`](e);
+  },
+};
+
+const noop = event => event.preventDefault();
+
+const clickHandlerFactory = (clickHandler = noop) =>
+  Object.create(EventHandler, {
+    onclick: {
+      value: clickHandler,
+    },
+  });
+
+// TODO injection of document and functions
+const createButton = ({ handler = noop, text = 'run', classList = [] }) => {
+  const $button = document.createElement('button');
+  const $textNode = document.createTextNode(text);
+  const clickHandler = clickHandlerFactory(handler);
+
+  $button.classList.add(...classList);
+  $button.appendChild($textNode);
+  $button.addEventListener('click', clickHandler, true);
+
+  return $button;
+};
+
+const $root = document.querySelector('#root');
+
+const functionReferenceSymbol = Symbol.for('functionReference');
+
+const $runButton = createButton({
+  handler(event) {
+    event.preventDefault();
+    window[functionReferenceSymbol](console);
+  },
+});
+
+$root.appendChild($runButton);
---
+++ src/symbol_examples.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-console */
+
+/*
+ * Part 1: Symbols
+ * Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol
+ * CodePen Link: https://codepen.io/Taar/pen/RyxmYr/
+*/
+
+/* eslint-disable-next-line no-unused-vars */
+const SymbolExample = (console) => {
+  // A Symbol factory only accepts one option parameter, a description which is
+  // only helpful for debugging.
+  // The string given as the description cannot be used for accessing a symbol.
+  const mySymbol = Symbol('mySymbol');
+  const mySymbolTwo = Symbol('mySymbolTwo');
+
+  // Symbols are unqiue!
+  console.log(mySymbol === mySymbolTwo);
+
+  // Symbol is a Factory function, therefore you can't use it with the new keyword
+  try {
+    // eslint-disable-next-line
+    new Symbol();
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  // If you need a global symbol you can register it in the Global registry
+  // `Symbol.for` will create a new symbol if it doesn't already exist
+  const myGlobalSymbol = Symbol.for('myGlobalSymbol');
+  // You can retrieve it by using the key given to `Symbol.for`
+  console.log(myGlobalSymbol === Symbol.for('myGlobalSymbol'));
+  // If you have a reference to a symbol and what to know it's key in the global registry
+  console.log(Symbol.keyFor(myGlobalSymbol));
+
+  // There are also default Symbols you can use like Symbol.iterator or Symbol.match
+  // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol#Properties
+
+  // Why are Symbols are useful?
+  // To best answer this question is to provide some code examples
+
+  const Person = (() => {
+    const invited = Symbol('invited');
+
+    return class Person_ {
+      constructor({ name, invite = false }) {
+        this.name = name;
+        this[invited] = invite;
+      }
+
+      set isInvited(inviteStatus) {
+        this[invited] = inviteStatus;
+      }
+
+      get isInvited() {
+        return this[invited];
+      }
+
+      // Works with Object.prototype.toString()
+      get [Symbol.toStringTag]() {
+        return this.name;
+      }
+    };
+  })();
+
+  const dwight = new Person({ name: 'Dwight', invite: true });
+  console.log(dwight.isInvited);
+  console.log(dwight.toString(), `${dwight}`);
+  // JSON.stringify() ignores Symbol-keyed properties
+  console.log(JSON.stringify(dwight));
+  // Doesn't show Symbol-keyed properties
+  console.log(Object.getOwnPropertyNames(dwight));
+  // Only get Symbol-keyed properties
+  console.log(Object.getOwnPropertySymbols(dwight));
+};
+
+window[Symbol.for('functionReference')] = SymbolExample;
